1fd294b740bbf08eefcfd051c831a76698885157
GuilhermeLCK <guilhermelimacosta@gmail.com>
2023-06-05 22:41:45 -0300

    ajuste mobile gui

--- src/components/HeaderHome/index.tsx
+++ src/components/HeaderHome/index.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import styles from '../../styles/Header.Home.module.scss';
-import SearchInput from '../InputSearch';
-import { useState } from 'react';
-import Link from 'next/link';
+import Head from "next/head";
+import styles from "../../styles/Header.Home.module.scss";
+import SearchInput from "../InputSearch";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function HeaderHome() {
-    const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
   return (
     <>
       <Head>
@@ -15,32 +15,50 @@ export default function HeaderHome() {
         `}</style>
       </Head>
       <div className={styles.HeaderContainer}>
-      
         <div className={styles.HeaderSearch}>
-          <Link href="/HomeResults">
-          <SearchInput></SearchInput>
-          </Link>
-          <div className={styles.HeaderContainerIcons}>
-              <img src='./iconSino.png' className={styles.HeaderIcon} height={20} width={20} />
-              <img src='./iconProfile.png' className={styles.HeaderIcon}  height={20} width={20} />
-              <img src='./iconInterro.png' className={styles.HeaderIcon}  height={20} width={20} />
+          <div className={styles.menuSamduba}>
+            {" "}
+            <img src="./menuSamduba.png" height={20} width={20} alt="" />
           </div>
 
-        </div>    
+          <Link href="/HomeResults">
+            <SearchInput></SearchInput>
+          </Link>
+          <div className={styles.HeaderContainerIcons}>
+            <img
+              src="./iconSino.png"
+              className={styles.HeaderIcon}
+              height={20}
+              width={20}
+            />
+            <img
+              src="./iconProfile.png"
+              className={styles.HeaderIcon}
+              height={20}
+              width={20}
+            />
+            <img
+              src="./iconInterro.png"
+              className={styles.HeaderIcon}
+              height={20}
+              width={20}
+            />
+          </div>
+        </div>
         <div className={styles.HeaderTextContainer}>
-            <div className={styles.HeaderTextTitle}>
-                Bem vindo de volta, <b>Evandro</b>
-            </div>
-            
-        </div>       
+          <div className={styles.HeaderTextTitle}>
+            Bem vindo de volta, <b>Evandro</b>
+          </div>
+        </div>
         <div className={styles.HeaderTextContainer}>
-            <div className={styles.HeaderTextDescription}>
-              <p>Aqui você pode realizar ações de forma rápida e fácil ou buscar por outras funcionalidades que esteja precisando </p>
-            </div>
-            
-        </div>       
-       
+          <div className={styles.HeaderTextDescription}>
+            <p>
+              Aqui você pode realizar ações de forma rápida e fácil ou buscar
+              por outras funcionalidades que esteja precisando{" "}
+            </p>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
--- src/components/HeaderHomeResults/index.tsx
+++ src/components/HeaderHomeResults/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head';
-import styles from '../../styles/Header.Home.Result.module.scss';
-import SearchInput from '../InputSearch';
-import { useState } from 'react';
+import Head from "next/head";
+import styles from "../../styles/Header.Home.Result.module.scss";
+import SearchInput from "../InputSearch";
+import { useState } from "react";
 
 export default function HeaderHomeResult() {
-    const [searchText, setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
   return (
     <>
       <Head>
@@ -14,23 +14,36 @@ export default function HeaderHomeResult() {
         `}</style>
       </Head>
       <div className={styles.HeaderContainer}>
-      
         <div className={styles.HeaderSearch}>
+          <div className={styles.menuSamduba}>
+            <img src="./menuSamduba.png" height={20} width={20} alt="" />
+          </div>
           <SearchInput></SearchInput>
           <div className={styles.HeaderContainerIcons}>
-              <img src='./iconSino.png' className={styles.HeaderIcon} height={20} width={20} />
-              <img src='./iconProfile.png' className={styles.HeaderIcon}  height={20} width={20} />
-              <img src='./iconInterro.png' className={styles.HeaderIcon}  height={20} width={20} />
+            <img
+              src="./iconSino.png"
+              className={styles.HeaderIcon}
+              height={20}
+              width={20}
+            />
+            <img
+              src="./iconProfile.png"
+              className={styles.HeaderIcon}
+              height={20}
+              width={20}
+            />
+            <img
+              src="./iconInterro.png"
+              className={styles.HeaderIcon}
+              height={20}
+              width={20}
+            />
           </div>
-
-        </div>    
+        </div>
         <div className={styles.HeaderTextContainer}>
-            <div className={styles.HeaderTextTitle}>
-                Resultado da busca:
-            </div>
-            
-        </div>              
+          <div className={styles.HeaderTextTitle}>Resultado da busca:</div>
+        </div>
       </div>
     </>
-  )
+  );
 }
