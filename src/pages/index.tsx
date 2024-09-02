21760046631c68896c236e092e9219ecda45fad9
João Pedro Batista Santos <joaopbatistastos@gmail.com>
2024-09-02 09:17:29 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -111,9 +111,10 @@ export default function Login() {
   return (
     <>
       <Head>
-        <style>{`
-          @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
-        `}</style>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
         <title>Rede Plug</title>
       </Head>
 
